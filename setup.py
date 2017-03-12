e554cbf94c06dd3af5816588fb728cdb15e72abd
Galileo Sartor <galileo.sartor@gmail.com>
2017-03-12 17:43:08 +0100

    Add icon

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(name = 'grive-indicator',
       version = '1.0',
       packages = ['grive_indicator'],
       package_data = {'grive_indicator': data},
+      data_files = [('/usr/share/applications', ['grive-indicator.desktop']),
+                    ('/usr/share/pixmaps/', ['web-google-drive.svg'])],
       scripts = ['bin/grive-indicator']
       )
