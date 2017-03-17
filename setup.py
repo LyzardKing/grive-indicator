8c22075bee5328fc74662558490701d4337825ab
Galileo Sartor <galileo.sartor@gmail.com>
2017-03-17 10:31:13 +0100

    Add proper python entry_point

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from distutils.core import setup
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
 
 data = ["data/*"]
 
-setup(name='grive-indicator',
+setup(
+      name='grive-indicator',
       version='1.0',
+      url='https://github.com/LyzardKing/grive-indicator',
+      author='Galileo Sartor',
+      author_email='galileo.sartor@gmail.com',
       packages=['grive_indicator'],
       package_data={'grive_indicator': data},
       data_files=[('/usr/share/applications', ['grive-indicator.desktop']),
                   ('/usr/share/pixmaps/', ['web-google-drive.svg'])],
-      scripts=['bin/grive-indicator']
-      )
+      entry_points={
+            'console_scripts': [
+                  'grive-indicator = grive_indicator:main',
+            ]
+      }
+)
